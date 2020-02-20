7c04b86433908861b40057f1b01d28548ac2f6ab
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-20 18:26:03 +0530

    [Divya]Add: created theabstract class unit and subclasses are implemented through inheritance

---
+++ qunatitymeasurment.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+# Abstract class for length
+class Unit(ABC):
+    def __init__(self, conversion_factor):
+        self.conversion_factor = conversion_factor
+
+    def comparisons_to_base(self, value):
+        return value*self.conversion_factor
+
+
+# Derived class from base class
+class InchUnit(Unit):
+    def __init__(self):
+        super().__init__(1)
+
+
+# Derived class from base class
+class FeetUnit(Unit):
+    def __init__(self):
+        super().__init__(12)
