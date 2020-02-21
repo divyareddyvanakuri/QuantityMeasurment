e4b5fde3eb574ae12417086c41c4278075fea02c
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-21 10:02:14 +0530

    [Divya]refactor: Implemented the inherited subclasses  inch ,feet and centimeter

--- qunatitymeasurment.py
+++ qunatitymeasurment.py
@@ -10,13 +10,19 @@ class Unit(ABC):
         return value*self.conversion_factor
 
 
-# Derived class from base class
+# Derived class from the base class
 class InchUnit(Unit):
     def __init__(self):
-        super().__init__(5)
+        super().__init__(1)
 
 
-#Derived class from base class
+# Derived class from the base class
+class FeetUnit(Unit):
+    def __init__(self):
+        super().__init__(12)
+
+
+# Derived class from the base class
 class CentiMeterUnit(Unit):
     def __init__(self):
-        super().__init__(2)
+        super().__init__(2/5)
