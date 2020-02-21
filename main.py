abfbec153fdb963fa994a49424701ac6a86514bc
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-21 10:15:45 +0530

    [Divya]refactor: Implemented the __eq__ method and created usecases is 1 gallon equals 3.78 litres and is 1 litre euqls 1000ml

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from qunatitymeasurment import CentiMeterUnit,FeetUnit,InchUnit,Unit
+from qunatitymeasurment import LitreUnit,GallonUnit,MlUnit,Unit
 
 
 #This is the composition class for feet and inch objects
@@ -7,36 +7,23 @@ class Quantity:
         self.value = value
         self.unit = unit
 
-    def __add__(self, other):
-        self.factor = int(self.unit.comparisons_to_base(self.value))
-        other.factor = int(other.unit.comparisons_to_base(other.value))
-        return self.factor+other.factor
+    def __eq__(self,other):
+        self.factor = float(self.unit.comparisons_to_base(self.value))
+        other.factor = float(other.unit.comparisons_to_base(other.value))
+        if self.factor == other.factor:
+            return True
+        return False
 
  
 try:
-   # 2inch+2inch
-    first_inch_objcet = Quantity(2, InchUnit())
-    second_inch_object = Quantity(2, InchUnit())
-    z = first_inch_objcet+second_inch_object
-    print("add two lengths 2inch and 2inch in inches:", z)
-
-    # 1ft+2inch
-    one_feet_object = Quantity(1, FeetUnit())
-    two_inch_objcet = Quantity(2, InchUnit())
-    z = one_feet_object+two_inch_objcet
-    print("add two lengths  1ft and 2inch in inches:", z)
-
-    # 1ft+1ft
-    first_feet_object = Quantity(1, FeetUnit())
-    second_feet_object = Quantity(1, FeetUnit())
-    z = first_feet_object+second_feet_object
-    print("add two lengths  1ft and 1ft in inches:", z)
-
-    # 2inch+2.5cm
-    two_inch_objcet = Quantity(2, InchUnit())
-    first_inch_object = Quantity(2.5, CentiMeterUnit())
-    z = two_inch_objcet+first_inch_object
-    print("add two lengths  2inch and 2.5cm in inches:", z)
+  # 1 gallon = 3.78 litres
+    gallon_object = Quantity(1,GallonUnit())
+    litres_object = Quantity(3.78,LitreUnit())
+    print("Is 1 gallon = 3.78 litres:",gallon_object == litres_object)
+    # 1 litre = 1000 ml
+    litre_object = Quantity(1,LitreUnit())
+    ml_object = Quantity(1000,MlUnit())
+    print("Is 1 litre = 1000 ml:",litre_object == ml_object)
 except NameError as e:
     print("Runtime Error:",e)
 except ValueError as e:
